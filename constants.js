484d8af6db7d73492c51d1a6f1eb9e3f5d3a8217
Sarim Ali <sarimali8549@gmail.com>
2025-07-21 13:11:15 +0530

    feat: Update resume download link

--- constants.js
+++ constants.js
@@ -7,6 +7,6 @@ export const SECTION_IDS = {
   CONTACT: 'contact',
 };
 
-export const RESUME_DOWNLOAD_LINK = "#"; 
+export const RESUME_DOWNLOAD_LINK = "https://github.com/sarim-aliii/Portfolio/blob/main/Sarim_resume.pdf"; 
 export const PHOTO_URL = "https://avatars.githubusercontent.com/u/145320874?v=4";
 export const PROJECT_PLACEHOLDER_IMAGE_BASE = "https://placehold.co/600x400";
